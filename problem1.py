3f0cb41b921a80114f6c35386e050b58154eb49f
h-weijie <1713425550@qq.com>
2019-09-15 09:34:06 +0800

    v2 faster

--- problem1.py
+++ problem1.py
@@ -1,6 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(0,len(nums)):
-            for j in range( i+1,len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [ i, j ]
+        dict={}
+        for i in range(len(nums)):
+            if target-nums[i] not in dict:
+                dict[nums[i]] = i
+            else:
+                return dict[target-nums[i]],i
