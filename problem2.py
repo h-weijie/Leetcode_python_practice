57c2fbef44d1b3167fa693b94fd3bdc90f24f2bc
h-weijie <1713425550@qq.com>
2019-09-15 16:46:06 +0800

    v2 readable

--- problem2.py
+++ problem2.py
@@ -6,26 +6,17 @@
 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        result = l1
-        while l1.next and l2.next:
-            l1.val += l2.val
-            l1 = l1.next
-            l2 = l2.next
-        if l1.next:
-            l1.val += l2.val
-        else:
-            l1.val += l2.val
-            l1.next = l2.next
+        result = ListNode(0)
         carry = 0
-        l1 = result
-        while l1.next:
-            temp = l1.val + carry
-            carry = temp // 10
-            l1.val = temp % 10
-            l1 = l1.next
-        temp = l1.val + carry
-        carry = temp // 10
-        l1.val = temp % 10
-        if carry:
-            l1.next = ListNode(1)
-        return result
+        p = result
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            p.next = ListNode(carry%10)
+            p = p.next
+            carry //= 10
+        return result.next
