9583db0cdab61638e43378d83fa697dc90cb99b1
h-weijie <1713425550@qq.com>
2019-09-20 20:22:17 +0800

    v2 more readable

--- problem45.py
+++ problem45.py
@@ -1,19 +1,9 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        queue = [0]
-        end = len(nums)
-        i = 1 
-        length = last = 0
-        while i<end:
-            start = queue[0]
-            if i <= start + nums[start]:
-                queue.append(i)
-                i += 1
-            else:
-                if last == queue.pop(0):
-                    last = queue[-1]
-                    length += 1
-            if i == end:
-                length += 1
-                break
-        return length
+        jumpnum = current_point = max_point = 0
+        for i in range(len(nums)):
+            if i > current_point:
+                current_point = max_point
+                jumpnum +=1
+            max_point = max(max_point,i+nums[i])
+        return jumpnum
